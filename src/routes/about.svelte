33a5c92c3a38211348ac57f2b3321d6b7b5d0c6f
Numan Arshad <numanuet311@gmail.com>
2020-05-02 16:07:30 +0500

    sapper svelte basic project setup

---
+++ src/routes/about.svelte
@@ -0,0 +1,19 @@
+<script>
+import { goto } from '@sapper/app';
+ function nav(){
+	alert("here")
+   goto("/blog")
+}
+
+
+
+</script>
+
+
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<h1>About this site</h1>
+<button on:click={nav}>d</button>
+<p>This is the 'about' page. There's not much here.</p>
---
+++ src/routes/login.svelte
@@ -0,0 +1,9 @@
+<script>
+import { goto } from '@sapper/app';
+ function nav(){
+	alert("here")
+   goto("/about")
+}
+</script>
+<h1>afksnvd</h1>
+<button on:click={nav}>d</button>
